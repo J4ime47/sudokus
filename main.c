83ac0659b5a5c834087259c08885b82250e40035
J4ime47 <jaimejimenezperez129@gmail.com>
2026-07-05 13:10:49 +0200

    chiquicambios

--- main.c
+++ main.c
@@ -5,10 +5,10 @@
 int main(){
     System systema;
     Sudoku candidate;
-    int i,j,admitidos,total_steps,num_unfixed,icords[2],jcords[2],delta_En,repetidos,prev_energy;
+    int i,j,admitidos,total_steps,num_unfixed,icords[2],jcords[2],delta_En,repetidos,prev_energy,resets;
     double beta,ttot,admitancia;
     clock_t inicio,final,timeo;
-    //FILE *f =fopen("results/medio.txt","w");
+    //FILE *f =fopen("results/facil.txt","w");
     //if(f==NULL) return 1;
     timeo=time(NULL);
     
@@ -18,17 +18,17 @@ int main(){
     
 
     //Inicializamos el sudoku (Uso uno de ejemplo de internet)
-    int pregunta[9][9] = {
-        {0,2,0,7,0,0,0,0,0},
-        {0,0,0,0,0,1,3,0,0},
-        {0,0,0,0,8,0,2,0,1},
-        {0,1,0,0,5,8,9,0,3},
-        {0,5,0,0,9,0,0,0,0},
-        {4,0,0,0,0,0,0,0,0},
-        {0,0,8,0,1,0,0,0,9},
-        {0,0,0,9,3,2,0,5,0},
-        {7,0,0,0,0,0,0,0,0}
-    };
+  int pregunta[9][9] = {
+    {0,0,0,0,5,0,3,0,0},
+    {7,0,0,2,4,0,0,0,0},
+    {0,0,0,0,3,0,6,0,2},
+    {5,0,2,3,9,0,0,6,0},
+    {0,3,6,8,7,0,5,0,0},
+    {0,0,0,0,6,0,7,0,4},
+    {0,0,7,4,0,0,0,5,3},
+    {0,0,8,0,0,5,4,1,6},
+    {2,0,0,0,0,0,8,0,7}
+};
 
     //Copiamos la matriz de la pregunta en el sudoku
     for(i=0;i<dim;i++){
@@ -41,7 +41,7 @@ int main(){
 
     //Inicializamos el sistema
     systema.beta_initial=0.01;
-    systema.termalization_steps=5000;
+    systema.termalization_steps=100;
     systema.beta_step=0.01;
 
 
@@ -59,6 +59,7 @@ int main(){
     printf("\nEnergia inicial: %d, GL=%i, SEMILLA=%ld\n", systema.energy,num_unfixed,timeo);
     wait();
     //Contador de iteraciones exactas
+    resets=0;
     total_steps = 0;
     repetidos = 0;
     prev_energy = systema.energy;
@@ -89,14 +90,15 @@ int main(){
             repetidos = 0;
         }
 
-        if(repetidos >= 20){
-            printf("Energia estancada en %d, reiniciando...\n", systema.energy);
-            initialize_sudoku(&systema.sudoku);
-            copy_sudoku(systema.sudoku,&candidate);
+        //Si se queda atascado en un minimo local, comenzamos del principio
+        if(repetidos >= 15){
+            //initialize_sudoku(&systema.sudoku);
+            //copy_sudoku(systema.sudoku,&candidate);
             systema.energy=energy(systema.sudoku);
             prev_energy = systema.energy;
             repetidos = 0;
             beta = systema.beta_initial;
+            resets++;
         }
 
         printf("Energia=%i , Admitancia=%.1f%%\n",systema.energy,admitancia);
@@ -113,7 +115,7 @@ int main(){
     printf("--SUDOKU FINAL--\n");
     print_solution(systema.sudoku);
     //fprintf(f,"%.2f %i %.2f\n", beta, systema.energy,admitancia);
-    printf("beta=%.2f, Energia=%i, Admitancia=%.2f%%.\n", beta, systema.energy,admitancia);
+    printf("beta=%.2f, Energia=%i, Admitancia=%.2f%% Resets=%i.\n", beta, systema.energy,admitancia,resets);
     printf("Se han hecho %d iteraciones.\n", total_steps);
     printf("El programa ha tardado %.3f segundos\n\n", ttot);
     //fclose(f);
